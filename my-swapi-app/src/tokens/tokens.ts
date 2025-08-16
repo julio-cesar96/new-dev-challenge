7b9d671a720efc762363dff4361ac8597bfa17ec
julio-cesar96 <julio_cbs96@hotmail.com>
2025-08-16 17:56:04 -0300

    feat: add tokens colors

---
+++ my-swapi-app/src/tokens/tokens.ts
@@ -0,0 +1,21 @@
+// src/styles/tokens.ts
+export const colors = {
+  primary: "#6666ff",
+  secondary: "#ff66ff",
+  success: "#28a745",
+  error: "#dc3545",
+  warning: "#ffc107",
+  white: "#ffffff",
+  black: "#000000",
+  gray: {
+    100: "#f8f9fa",
+    200: "#e9ecef",
+    300: "#dee2e6",
+    400: "#ced4da",
+    500: "#adb5bd",
+    600: "#6c757d",
+    700: "#495057",
+    800: "#343a40",
+    900: "#212529",
+  },
+};
